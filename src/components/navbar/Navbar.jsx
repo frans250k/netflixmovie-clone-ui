d7070beff3f1064c471b34bdaa861a652a7032c2
Frans Kbarek <franzferdinand.kbarek@gmail.com>
2021-09-27 16:04:39 +0900

    component featured fixed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,9 +9,8 @@ const Navbar = () => {
     setIsScrolled(window.pageYOffset === 0 ? false : true);
     return () => (window.onscroll = null);
   };
-
   return (
-    <div className={isScrolled ? "navbar-scrolled" : "navbar"}>
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
       <div className="container">
         <div className="left">
           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt="" />
