c34ccaa691465b83d7826fe363787b428e8604c0
Frans Kbarek <franzferdinand.kbarek@gmail.com>
2021-10-09 14:19:20 +0900

    add register and login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Home from "../src/pages/home/Home";
 import Watch from "../src/pages/watch/Watch";
+import Register from "../src/pages/register/Register";
+import Login from "../src/pages/login/Login";
 import "./app.scss";
 
 const App = () => {
   return (
     <div>
-      <Watch />
+      <Login />
     </div>
   );
 };
